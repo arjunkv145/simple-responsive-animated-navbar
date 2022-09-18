5bd8eb8a0ea123700f49a50541a3874beaa51f58
arjun k v <arjunvijayan470@gmail.com>
2022-09-18 19:26:39 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('#nav-menu-container').addEventListener('click', () => {
+    document.querySelector('#nav-links').classList.toggle('open')
+    document.querySelector('#nav-menu').classList.toggle('open')
+})
